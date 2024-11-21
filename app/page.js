8c82968cd596b8416ec45678a2cbb8924dcdd08c
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2024-11-21 16:17:09 +0530

    c 1

--- app/page.js
+++ app/page.js
@@ -1,101 +1,151 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+export default function MeterCard() {
+  const [performance, setPerformance] = useState(61);
+  const [availability, setAvailability] = useState(52);
+  const [quality, setQuality] = useState(34);
+
+  // Calculate the average
+  const average = ((performance + availability + quality) / 3).toFixed(1);
+
+  const data = {
+    labels: ["Performance", "Availability", "Quality", "a"],
+    datasets: [
+      {
+        // Performance
+        data: [performance, 100 - performance],
+        backgroundColor: ["#049C64", "#33363F"],
+        borderWidth: 0,
+      },
+      {
+        // Availability
+        data: [availability, 100 - availability],
+        backgroundColor: ["#FF0004", "#33363F"],
+        borderWidth: 0,
+      },
+      {
+        // Quality
+        data: [quality, 100 - quality],
+        backgroundColor: ["#FFBB38", "#33363F"],
+        borderWidth: 0,
+        borderRadius: 3,
+      },
+      {
+        // empty
+        data: [100],
+        backgroundColor: ["#33363F", "#33363F"],
+        borderWidth: 0,
+        cutout: "60%",
+
+      },
+    ],
+  };
+
+  const options = {
+    rotation: -90,
+    circumference: 180,
+    cutout: "60%",
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: (tooltipItem) => {
+            const { datasetIndex, dataIndex } = tooltipItem;
+            const labels = ["Performance", "Availability", "Quality"];
+            const values = [performance, availability, quality];
+            if (dataIndex === 0) {
+              return `${labels[datasetIndex]}: ${values[datasetIndex]}%`;
+            }
+            return null;
+          },
+        },
+      },
+      centerText: {
+        display: true,
+        text: `\n\n${average}%\n\nOEE`,
+      },
+    },
+  };
+
+  //  plugin for center text 
+  ChartJS.register({
+    id: "centerText",
+    beforeDraw: (chart) => {
+      const { ctx, width, height } = chart;
+      const { text } = chart.config.options.plugins.centerText;
+
+      // Draw white circle in the center
+      ctx.save();
+      ctx.fillStyle = "transparent"; // White background for the center
+      ctx.beginPath();
+      ctx.arc(width / 2, height / 2, height / 4.5, 0, Math.PI * 2); // Adjust radius
+      ctx.fill();
+      ctx.restore();
+
+      // Draw center text
+      if (text) {
+        ctx.save();
+        ctx.font = "900 32px Arial";
+        ctx.fillStyle = "#ffffff";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+
+        const lines = text.split("\n");
+        lines.forEach((line, index) => {
+          ctx.fillText(line, width / 2, height / 2 + index * 20);
+        });
+        ctx.restore();
+      }
+    },
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col items-center w-full">
+      <div className=" flex flex-col justify-between bg-meter-box-gradient shadow-lg rounded-lg px-2 pt-2 w-1/2 ">
+        <h1 className="text-2xl mr-auto font-bold text-[#333333]">OEE</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        {/*  Boxes */}
+        <div className="flex justify-around w-full ">
+          <div className="flex flex-col items-center">
+            <span className="text-xl text-[#6E7075] font-bold mb-1"> Performance</span>
+            <div className=" py-1 px-3 text-white font-bold bg-percentage-box-gradient rounded-full text-center">
+              <ArrowDropUpIcon sx={{ fontSize: 40 }} /> {performance.toFixed(2)}%
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-xl text-[#6E7075] font-bold mb-1">Availability</span>
+            <div className="py-1 px-3 text-white font-bold bg-availability-box-gradient rounded-full text-center">
+              <ArrowDropUpIcon sx={{ fontSize: 40 }} /> {availability.toFixed(2)}%
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-xl text-[#6E7075] font-bold mb-1">Quality</span>
+            <div className="py-1 px-3 text-white font-bold bg-quality-box-gradient rounded-full text-center">
+              <ArrowDropUpIcon sx={{ fontSize: 40 }} /> {quality.toFixed(2)}%
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Doughnut Chart */}
+        <div className="  w-[500px]  mx-auto h-[500px]">
+          <Doughnut data={data} options={options} />
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'percentage-box-gradient': 'radial-gradient(70% 437.5% at 100% 50%, #86FFD2 0%, #049C64 47.89%)',
+        'availability-box-gradient': 'radial-gradient(70% 437.5% at 100% 50%, #FF9FA1 0%, #FF0004 47.89%)',
+        'quality-box-gradient': 'radial-gradient(70% 437.5% at 100% 50%, #F7E2E2 0%, #FFBB38 47.89%)',
+        'meter-box-gradient': 'linear-gradient(270deg, rgba(255, 255, 255, 0.9) 0%, rgba(235, 235, 235, 0.9) 100%)',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
